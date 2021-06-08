3ede9339872dbc83c976e8d8cf54aa7437b090ad
wch <wanicca@163.com>
2021-06-08 18:55:20 +0800

    Modified get_parts in AtomicPVP

--- pet/pvp.py
+++ pet/pvp.py
@@ -634,10 +634,12 @@ class AtomicPVP(PVP):
 
     def get_parts(self, example: InputExample) -> FilledPattern:
         # switch text_a and text_b to get the correct order
+        if not hasattr(self,"max_label_tokens"): 
+            self.max_label_tokens = max(len(get_verbalization_ids(l[0],self.wrapper.tokenizer, False)) for l in self.VERBALIZER.values())
+
         text_a = example.text_a
         text_b = example.text_b.rstrip(string.punctuation)
-        return ['"', self.shortenable(text_a), '";'], [[self.mask], '"', self.shortenable(text_b), '"'
-]
+        return ['"', self.shortenable(text_a), '";', self.mask * self.max_label_tokens, '"', self.shortenable(text_b), '"'], []
 
     def verbalize(self, label) -> List[str]:
         return AtomicPVP.VERBALIZER[label]
